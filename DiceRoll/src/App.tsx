456441bb408c1413526de7c1626c2f308ec3a1a4
Jain K <kuriakosejain456@gmail.com>
2023-12-20 20:01:34 +0530

    Added assets , created module

---
+++ DiceRoll/src/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import DiceOne from '../assets/One.png'
+import DiceTwo from '../assets/Two.png'
+import DiceThree from '../assets/Three.png'
+import DiceFour from '../assets/Four.png'
+import DiceFive from '../assets/Five.png'
+import DiceSix from '../assets/Six.png'
+
+
+function App(): JSX.Element {
+
+  return (
+    <View>
+      <Text>Text</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
+
+
+export default App;
