89f85196af8a8f129720330dee87ed1a3549bdf4
Jain K <kuriakosejain456@gmail.com>
2023-12-21 21:48:55 +0530

    added styling and props to btn

--- CurCon/src/components/CurBtn.tsx
+++ CurCon/src/components/CurBtn.tsx
@@ -1,10 +1,40 @@
 import React from "react";
+import type { PropsWithChildren } from "react";
 
-const CurBtn = () => {
-    return (
-        <div>Currency Button</div>
+import {
+    View, 
+    Text,
+    StyleSheet,
+} from 'react-native'
+
+type CurBtnProps = PropsWithChildren<{
+    name: string;
+    flag: string;
+}>
+
+const CurBtn = (props: CurBtnProps): JSX.Element => {
+    return(
+        <View style={styles.BtnContainer}>
+            <Text style={styles.flag}>{props.flag}</Text>
+            <Text style={styles.country}>{props.name}</Text>
+        </View>
     )
-} 
+}
 
+const styles = StyleSheet.create({
+    BtnContainer :{
+        alignItems: 'center'
+    },
+    flag :{
+        fontSize: 28,
+        color:"#ffffff",
+        marginBottom: 4
+    },
+    country : {
+        fontSize: 14,
+        color:"#28ff20",
+        marginBottom: 4
+    }
+})
 
 export default CurBtn
