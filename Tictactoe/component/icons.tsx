3ea084e33c9adb4663e4372adcfef1e78e9c85f4
Jain K <kuriakosejain456@gmail.com>
2023-12-26 19:43:35 +0530

    Added fontAwesome types

--- Tictactoe/component/icons.tsx
+++ Tictactoe/component/icons.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import type { PropsWithChildren } from 'react'
+import { Switch } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome'
 
+type IconsProp = PropsWithChildren <{
+    name : string;
+}>
 
-function icons() {
-  return (
-    <div>icons</div>
-  )
+const icons = ({name}) => {
+switch (name) {
+    case 'circle':
+        return <Icon name="circle-thin" size={38} color = "#F7CD2E" ></Icon>
+        break;
+
+    default:
+        break;
+}
 }
 
 export default icons
