a0bdbdf7c983e8f39a264a9ad872352f94c26160
Jain K <kuriakosejain456@gmail.com>
2023-12-26 19:38:37 +0530

    Added icon components

---
+++ Tictactoe/component/icons.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import type { PropsWithChildren } from 'react'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+
+function icons() {
+  return (
+    <div>icons</div>
+  )
+}
+
+export default icons
